d2e2a2b2bd54f63cdeda2755467443c66fdae0f2
ccoralie <coraliecotte.cc@gmail.com>
2018-05-14 16:35:25 +0200

    routing

---
+++ src/AppBundle/Controller/ReviewController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+
+use AppBundle\Entity\Review;
+
+
+/**
+ * Listing controller.
+ *
+ * @Route("review")
+ */
+class ReviewController extends Controller
+{
+
+
+    /**
+     * Lists all review entities.
+     *
+     * @Route("/", name="review_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $review = $em->getRepository('AppBundle:Review')->findAll();
+
+        return $this->render('review/index.html.twig', array(
+            'review' => $review,
+
+        ));
+    }
+
+    /**
+     * Creates a new review entity.
+     *
+     * @Route("/new", name="review_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction()
+    {
+        return $this->render('review/new.html.twig');
+    }
+
+}
